3ae8fc93a6c9837a8ffdc067fa4d2b8356fa4069
Mattthhhhh <angelika.christen@protonmail.com>
2025-10-24 10:14:29 +0200

    Update sizeof_types.c

---
+++ TP1/src/sizeof_types.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  printf, (sizeof(char));
+  printf, (sizeof(short));
+  printf, (sizeof(int));
+  printf, (sizeof(long int));
+  prinft, (sizeof(long long int));
+  printf, (sizeof(float));
+  printf, (sizeof(double));
+  printf, (sizeof(long double));
+return 0;
+}
+  
+  
