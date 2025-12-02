909d1ceb3ad92fb4a91b4460fb8e66c67788124c
Mattthhhhh <angelika.christen@protonmail.com>
2025-12-02 10:12:29 +0100

    pyramide.c

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n = 5; // Hauteur de la pyramide
+    int i, j, k;
+
+    for (i = 1; i <= n; i++) {
+        // Affichage des espaces pour centrer les nombres
+        for (j = 1; j <= n - i; j++) {
+            printf("  ");
+        }
+
+        for (k = 1; k <= i; k++) {
+            printf("%d ", k);
+        }
+
+        for (k = i - 1; k >= 1; k--) {
+            printf("%d ", k);
+        }
+
+        printf("\n");
+    }
+
+    printf("La génération de la pyramide est terminée.\n");
+
+    return 0;
+}
 
